dc879a7e34b5a391fe82dcea9fc53292e5b5ee00
HammerOfDane <37005361+HammerOfDane@users.noreply.github.com>
2018-03-05 14:25:07 -0500

    Add files via upload
    This version succesfully randomly generates a suduko puzzle, it also is able to solve a suduko puzzle given in form int y[9][3][3]. It is also now able to currently pass a level check through the desolver, which removes any number that can only go in one spot. Currently working on the level 2 check which will go through and test each number that can go at a spot and sees if it is solvable

--- Main.java
+++ Main.java
@@ -18,6 +18,11 @@ public class Main
       Suduko board = board = new Suduko();
       
       board.printBoard(board.x);
+      System.out.println("---------------------------------------");
+      board.printBoard(board.y);
+      
+      board.checkCells();
+      board.printBoardVals(board.possvals);
       
       return;
    
@@ -26,13 +31,242 @@ public class Main
    }
 
 class Suduko
-{
+{  
+   //allows use of random number
    Random rand = new Random();
+   //solved puzzle
    int x[][][] = genBoard();
+   //will be the unsolved puzzle
+   int y[][][] = genBoard();
+   //temp array holds the value of the unsolved puzzle while removing numbers
+   int w[][][] = genBoard();
+   //sets the list of possible numbers
+   int list[] = {1,2,3,4,5,6,7,8,9};
+   //sets a list that stores possible values for each position
+   int possvals[][][] = new int[9][9][9];
+   //stores number of solutions if you remove a number
+   int solutions = 0;
    
+   
+   //instantiates the class by generating the table
    public Suduko()
    {
       genNumbers();
+      
+      copyArray(x,y);
+      
+      desolver();
+      
+      
+   }
+   //compares arrays
+   public boolean compareArray(int[][][] u, int[][][] q)
+   {
+      for(int i = 0; i < 9; i += 1)
+      {
+            for(int j = 0; j < 3; j += 1)
+            {
+               for(int k = 0; k < 3; k+=1)
+               {
+                  if(u[i][j][k] == 0)
+                  {
+                  continue;
+                  }
+                  if(u[i][j][k] != q[i][j][k])
+                  {
+                  return false;
+                  }
+                  else
+                  {
+                  continue;
+                  }
+               }
+            }
+      }
+      return true;
+   }
+   
+   //sets all values for y to x values
+   public void copyArray(int[][][] u, int[][][] q)
+   {
+      for(int i = 0; i < 9; i += 1)
+      {
+            for(int j = 0; j < 3; j += 1)
+            {
+               for(int k = 0; k < 3; k+=1)
+               {
+                  q[i][j][k] = u[i][j][k];
+               }
+            }
+      }
+   }
+   
+   //counts and prints the number of cells remaining
+   public int checkCells()
+   {
+      int cells = 0;
+      
+      for(int i = 0; i < 9; i += 1)
+      {
+            for(int j = 0; j < 3; j += 1)
+            {
+               for(int k = 0; k < 3; k+=1)
+               {
+                  if(y[i][j][k] != 0)
+                  {
+                     cells += 1;
+                  }
+               }
+            }
+      }
+      System.out.println("Cells remaining: " + cells);
+      return cells;
+   }
+   
+   //checks to see if you can remove the value at current coordinates
+   public int checkValueLevI(int a, int b, int c)
+   {
+      boolean numberPass;
+      int numsi = 0;
+      y[a][b][c] = 0;
+      
+      
+      //tests each possible number in the spot          
+      for(int l = 0; l < 9; l +=1)
+      {
+         int p = list[l];
+         numberPass = testRow(y, p, a);
+         numberPass = numberPass & testBox(y, p, a, b);
+         numberPass = numberPass & testCol(y, p, b, c);
+         if(numberPass == true)
+         {
+            numsi+= 1;
+            if((b == 0) & (c == 0))
+            {
+               possvals[a][0][l] = p;
+            }
+            else if((b == 0))
+            {
+               possvals[a][c][l] = p;
+            }
+            else if((b == 1))
+            {
+               possvals[a][3+c][l] = p;
+            }
+            else if((b == 2))
+            {
+               possvals[a][6+c][l] = p;
+            }
+         }
+                          
+      }
+      if((numsi == 1)/* || (numsi == 2)*/)
+      {
+         return numsi;
+      }
+      else if(numsi > 1)
+      {
+         y[a][b][c] = x[a][b][c];
+         return numsi;
+      }
+      else if(numsi == 0)
+      {
+         y[a][b][c] = x[a][b][c];
+         return numsi;
+      }
+      return numsi;
+   }
+   
+   //Runs a higher level check on puzzle square
+   public void checkValueLevII(int a, int b, int c)
+   {
+      
+      int ind2 = 0;
+      
+      if((b == 0) & (c == 0))
+      {
+         ind2 = 0;
+      }
+      else if((b == 0))
+      {
+         ind2 = c;
+      }
+      else if((b == 1))
+      {
+         ind2 = 3 + c;
+      }
+      else if((b == 2))
+      {
+         ind2 = 6 + c;
+      }
+      
+      if(possvals[a][ind2].length == 2)
+      {
+         y[a][b][c] = 0;
+      }
+      
+      
+      solutions = 0;
+      do
+      {
+         for(int i = 0; i < possvals[a][ind2].length; i += 1)
+         {
+            y[a][b][c] = possvals[a][ind2][i];
+            loopednumbers(y);
+            if(compareArray(y,x) == true)
+            {
+               copyArray(w,y);
+               continue;
+            }
+            else if(compareArray(y,x) == false)
+            {
+               copyArray(w,y);
+            }
+         }
+      }while(solutions <= 1);
+      if(solutions == 1)
+      {
+         y[a][b][c] = 0;
+         copyArray(w,y);
+      }
+      else
+      {
+         y[a][b][c] = w[a][b][c];
+      }
+      
+      return;    
+   }
+   
+   //code to remove as many values as possible from puzzle
+   public void desolver()
+   {
+      //boolean solvable = true;
+      copyArray(y,w);
+      for(int i = 0; i < 500; i += 1)
+      {
+         int row = rand.nextInt(9);
+         int bigCol = rand.nextInt(3);
+         int lilCol = rand.nextInt(3);
+         
+         while(y[row][bigCol][lilCol] == 0)
+         {
+            row = rand.nextInt(9);
+            bigCol = rand.nextInt(3);
+            lilCol = rand.nextInt(3);
+         }
+         
+         checkValueLevI(row, bigCol, lilCol);
+         //checkValueLevII(row, bigCol, lilCol);
+         
+      
+      }
+      loopednumbers(w);
+      //copyArray(y,w);
+      printBoard(w);
+      //loopednumbers(y);
+      System.out.println("------------------------------------------");
+      //System.out.println(solutions);
+      return;
    }
    
    //generates an empty 1 x 3 row
@@ -68,14 +302,14 @@ class Suduko
       return u;   
    }
    
-   public void genNumbers()
+   //shifted the recursive loop from in gennumbers to its own so i could utilize elsewhere
+   //is technically a solver but recieved errors when trying to change name
+   public void loopednumbers(int t[][][])
    {
-      //creates the empty board
-       x = genBoard();
-      //sets the list of possible numbers
-      int list[] = {1,2,3,4,5,6,7,8,9};
       //counter for number of slots filled
       int nums= 0;
+      int b[][][] = genBoard();
+      copyArray(t,b);
 
       //format for array loop is [i][j][k]
       for(int i = 0; i < 9; i += 1)
@@ -84,6 +318,9 @@ class Suduko
          {
             for(int k = 0; k < 3; k+=1)
             {
+               //System.out.println(nums);
+               if(t[i][j][k] == 0)
+               {
                //sets the tests to false for each new spot
                boolean row = false;
                boolean box = false;
@@ -93,13 +330,14 @@ class Suduko
                // repeatededly trys numbers in spot until it finds one that fits
                do
                {  
+                  
                   //before testing numbers it goes through and sees if it is even possible to fill           
                   for(int l = 0; l < 9; l +=1)
                   {
                      int p = list[l];
-                     restart = testRow(x, p, i);
-                     restart = restart & testBox(x, p, i, j);
-                     restart = restart & testCol(x, p, j, k);
+                     restart = testRow(t, p, i);
+                     restart = restart & testBox(t, p, i, j);
+                     restart = restart & testCol(t, p, j, k);
                      if(restart == false)
                      {
                         restarti+= 1;
@@ -108,32 +346,48 @@ class Suduko
                   //if no number can go in the current spot it restarts
                   if(restarti == 9)
                   {
-                     genNumbers();
+                     //t = genBoard();
+                     copyArray(b,t);
+                     loopednumbers(t);
+                     return;
                   }
                   //picks random number and tests if it will work
                   int val = rand.nextInt(9) + 1;
-                  row = testRow(x, val, i);
-                  box = testBox(x, val, i, j);
-                  column = testCol(x, val, j, k);
+                  row = testRow(t, val, i);
+                  box = testBox(t, val, i, j);
+                  column = testCol(t, val, j, k);
                   //if the number works, it is assigned to the current slot
                   if(column && row && box)
                   {
                   nums += 1;
-                  x[i][j][k] = val;
+                  t[i][j][k] = val;
                   //below line showed progress of current iteration of loop
                   //System.out.println("The puzzle has " + nums + "/81 slots filled");
                   }
                   //if all slots are filled, loop ends
                   if(nums == 81)
                   {
+                     solutions += 1;
                      break;
                   } 
+                  
                }while(!column || !row || !box);
+               }
+               
             }
          }
       }
+   }
+   
+   public void genNumbers()
+   {
+      //creates the empty board
+       x = genBoard();
+
+      loopednumbers(x);
+      
       //code placed durign tests to ensure function reached return
-      printBoard(x);
+      //printBoard(x);
       return;
    }
    
@@ -312,6 +566,24 @@ class Suduko
             {
             System.out.print(a[i][j][k] + " ");
             }
+         }
+         System.out.println();
+      }
+   }
+   //prints board
+   public void printBoardVals(int a[][][])
+   {
+      for(int i = 0; i < 9; i += 1)
+      {
+         for(int j = 0; j < 9; j += 1)
+         {
+            System.out.print("[ ");
+            for(int k = 0; k < 9; k+=1)
+            {
+            if(a[i][j][k] != 0)
+            System.out.print(a[i][j][k] + " ");
+            }
+            System.out.print("]");
          }
          System.out.println();
       }
